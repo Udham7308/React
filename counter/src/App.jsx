89eb0cacc1f399c0bb26310db68e93fa604e2b7c
Udham Singh <129770745+Udham7308@users.noreply.github.com>
2024-07-09 12:23:33 +0000

    tailwind

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -13,7 +13,22 @@ function App() {
     if(counter >=20){
       stop;
     }else{
-      setCounter(counter+1);
+      //setCounter(counter+1);
+      // setCounter(counter+1);
+      // setCounter(counter+1);
+      // setCounter(counter+1); 
+      // UseState send all updates in batches. in upper task all update will become a batch which doing a same work. 
+      //So there will be only one update. on click counter's value will be 4 not 7.
+      // for this type task we define our setCounter as it's a call back fuction
+
+      setCounter(prevCounter=> prevCounter+1);
+      setCounter(prevCounter=> prevCounter+1);
+      setCounter(prevCounter=> prevCounter+1);
+      setCounter(prevCounter=> prevCounter+1);
+      //They all are not same because there states will be different based on previous state.
+      // now on update value will be 7. // prevCounter inputs it's previous value.
+
+      
     }
     
         
